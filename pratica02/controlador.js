5acea32b55b55a68387a898265fa0e73c2cd7f8c
Ingrid Maria <128629372+safeprogram@users.noreply.github.com>
2024-06-09 21:13:01 -0300

    Update controlador.js

---
+++ pratica02/controlador.js
@@ -0,0 +1,43 @@
+const Contato = require('./modelo');
+
+const contatos = []; 
+
+function adicionarContato(nome, email, telefone) {
+    const novoContato = new Contato(nome, email, telefone); 
+    contatos.push(novoContato); 
+}
+
+function listarContatos() {
+    return contatos; 
+}
+
+function buscarContato(nome) {
+    return contatos.find(contato => contato.nome === nome); 
+}
+
+function atualizarContato(nome, email, telefone) {
+    const contato = buscarContato(nome); 
+    if (contato) {
+        contato.email = email; 
+        contato.telefone = telefone; 
+        return true; 
+    }
+    return false; 
+}
+
+function removerContato(nome) {
+    const index = contatos.findIndex(contato => contato.nome === nome);
+    if (index !== -1) {
+        contatos.splice(index, 1); 
+        return true; 
+    }
+    return false; 
+}
+
+module.exports = {
+    adicionarContato,
+    listarContatos,
+    buscarContato,
+    atualizarContato,
+    removerContato
+};
